9bd05e7d6e2f4ca7fc16c3be54904f7fdc4f64d7
vannguyenn1999 <vannguyenn2809@gmail.com>
2026-06-27 00:03:49 +0700

    update cors

--- src/config/cors.js
+++ src/config/cors.js
@@ -12,7 +12,8 @@ const CORS_OPTIONS = {
     'http://localhost:3000',
     'http://127.0.0.1:5173',
     'http://127.0.0.1:3000',
-    // 'https://mindx-reactjs-final-test-kgd5.vercel.app'
+    'https://mindx-reactjs-final-test-kgd5.vercel.app',
+    'https://react-pay-gamma.vercel.app/'
   ],
   credentials: true,
   optionsSuccessStatus: 200,
