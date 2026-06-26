eca14b8b3e6c5c6d06ecea393cc9e45efce3a3d8
vannguyenn1999 <vannguyenn2809@gmail.com>
2026-06-27 00:07:01 +0700

    fix cors 2

--- src/config/cors.js
+++ src/config/cors.js
@@ -7,14 +7,15 @@
 import cors from 'cors';
 
 const CORS_OPTIONS = {
-  origin: [
-    'http://localhost:5173',
-    'http://localhost:3000',
-    'http://127.0.0.1:5173',
-    'http://127.0.0.1:3000',
-    'https://mindx-reactjs-final-test-kgd5.vercel.app',
-    'https://react-pay-gamma.vercel.app/'
-  ],
+  // origin: [
+  //   'http://localhost:5173',
+  //   'http://localhost:3000',
+  //   'http://127.0.0.1:5173',
+  //   'http://127.0.0.1:3000',
+  //   'https://mindx-reactjs-final-test-kgd5.vercel.app',
+  //   'https://react-pay-gamma.vercel.app/'
+  // ],
+  origin : true,
   credentials: true,
   optionsSuccessStatus: 200,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
